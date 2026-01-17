ee207eb8070e97b726fffedde7bd33d7019e40df
Pepe <perico.smely@gmail.com>
2026-01-17 14:02:13 -0300

    Initial commit: First Python program

---
+++ hello.py
@@ -0,0 +1,9 @@
+# My first Python program
+# Author: Carlos Cestac
+# Date: January 2025
+def greet(name):
+    return f"Hello, {name}! Welcome to QA Automation."
+if __name__ == "__main__":
+    print(gret("Carlos"))
+    print("Starting my QA automation learning journey!")
+    
